e7e83fc37be1c4073476b2f4ed2ad1973bca454f
Atul Kamble <kambleatul26@gmail.com>
2020-04-27 16:10:04 +0530

    Build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { WebIntent } from '@ionic-native/web-intent/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    WebIntent
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { WebIntent } from '@ionic-native/web-intent/ngx';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  payeeVPA: string;
+  payeeName: string;
+  transactionNote: string = 'Payment for Groceries';
+  payAmount: number;
+  currency: string = 'INR';
+  transactionReference: string;
+
+  constructor(private webIntent: WebIntent) {}
+
+  pay() {
+
+    this.payeeVPA = '7303718165@upi';
+    this.payeeName = 'Dhawal%20Mali';
+    this.payAmount = 20;
+    this.transactionReference = '#87148172'; //ORDER ID or Something similar
+
+    const url = 'upi://pay?pa=' + this.payeeVPA + '&pn=' + this.payeeName + '&tr=' + this.transactionReference + 'tn=' + this.transactionNote + '&am=' + this.payAmount + '&cu=' + this.currency;
+    const options = {
+      action: this.webIntent.ACTION_VIEW,
+      url
+    };
+    this.webIntent.startActivityForResult(options).then(success => {
+      console.log(success);
+      if(success.extras.Status == 'SUCCESS') {
+        // SUCCESS RESPONSE
+      } else if(success.extras.Status == 'SUBMITTED') {
+        // SUBMITTED RESPONSE
+      } else if(success.extras.Status == 'Failed' || success.extras.Status == 'FAILURE') {
+        // FAILED RESPONSE
+      } else {
+        // FAILED RESPONSE
+      }
+    }, error => {
+      console.log(error);
+    });
+  }
 
 }
